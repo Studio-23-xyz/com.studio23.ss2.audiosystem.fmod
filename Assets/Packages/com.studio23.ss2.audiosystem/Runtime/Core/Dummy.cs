85a7dbc609489eb2ce5cbef2bb8d135efb7ee7f6
Rasheeq Kamal <riksaber@gmail.com>
2023-12-04 18:36:38 +0600

    Tested performance between event instances and emitters

--- Assets/Packages/com.studio23.ss2.audiosystem/Runtime/Core/Dummy.cs
+++ Assets/Packages/com.studio23.ss2.audiosystem/Runtime/Core/Dummy.cs
@@ -7,26 +7,70 @@ using UnityEngine;
 public class Dummy : MonoBehaviour
 {
     public StudioEventEmitter emitter;
+    public int tempIndex;
+    public float time;
+    public bool startTime;
+    public int maxCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*AudioManager.Instance.CreateInstance(FMODBank_Sample.Test, gameObject);
-        AudioManager.Instance.Play(FMODBank_Sample.Test, gameObject);*/
-
+        //AudioManager.Instance.CreateInstance(FMODBank_Sample.Test, gameObject);
+        //AudioManager.Instance.Play(FMODBank_Sample.Test, gameObject);
         
+
     }
 
     [ContextMenu("Play")]
     public void Demo()
     {
-        AudioManager.Instance.CreateEmitter(FMODBank_Sample.Test, gameObject, emitter);
-        AudioManager.Instance.Play(FMODBank_Sample.Test, gameObject, emitter);
+        startTime = true;
+
+        for (tempIndex = 0; tempIndex <= maxCount; tempIndex++)
+        {
+            var instance = RuntimeManager.CreateInstance(FMODBank_Sample.Test);
+        }
+
+
+        /*StudioEventEmitter emitter2 = new StudioEventEmitter
+        {
+            EventReference = EventReference.Find(FMODBank_Sample.Test)
+        };
+        emitter2.EventInstance.start();*/
+
+        /*AudioManager.Instance.CreateEmitter(FMODBank_Sample.Test, gameObject, emitter);
+        AudioManager.Instance.Play(FMODBank_Sample.Test, gameObject, emitter);*/
     }
+
+    [ContextMenu("Play2")]
+    public void Demo2()
+    {
+        startTime = true;
+        for (tempIndex = 0; tempIndex <= maxCount; tempIndex++)
+        {
+            StudioEventEmitter emitter2 = new StudioEventEmitter
+            {
+                EventReference = EventReference.Find(FMODBank_Sample.Test)
+                
+            };
+            emitter2.Play();
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startTime)
+        {
+            time += Time.deltaTime;
+            if (tempIndex >= maxCount)
+            {
+                Debug.Log($"Time consumed: {time}");
+                tempIndex = 0;
+                startTime = false;
+                time = 0.0f;
+            }
+        }
     }
 }
