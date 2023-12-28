152ca9b9c9da7a0357003ac10e1d245db03f62a4
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2023-12-28 18:22:46 +0600

    Updated version

---
+++ Samples/Demo1/FMOD_Data/FMODBank_Dialogue.cs
@@ -0,0 +1,7 @@
+namespace Studio23.SS2.AudioSystem.fmod.Data
+{
+	public static class FMODBank_Dialogue
+	{
+		public static FMODEventData Dialogue_Dialogue = new FMODEventData("bank:/Dialogue", "event:/Dialogue/Dialogue", "{8e9f0b3d-5f42-422e-b43d-1b55fd43cdb2}");
+	}
+}
