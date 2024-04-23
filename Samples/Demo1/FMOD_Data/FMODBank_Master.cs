e373c450c1225bf0b1be785bed5fe8866a312912
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-04-23 17:51:39 +0600

    Updated the system to work without bank name

---
+++ Samples/Demo1/FMOD_Data/FMODBank_Master.cs
@@ -0,0 +1,14 @@
+using Studio23.SS2.AudioSystem.fmod.Data;
+
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODBank_Master
+	{
+		public static FMODEventData snapshot_Slow_Motion = new FMODEventData("snapshot:/Slow-Motion", "{6e8d8d09-12e0-48cf-b620-f9cc3bc478a3}");
+		public static FMODEventData snapshot_Health_Low = new FMODEventData("snapshot:/Health Low", "{87ea4134-fff1-4e9d-8f8b-294a2b518223}");
+		public static FMODEventData snapshot_Reverb_Sewer_Reverb = new FMODEventData("snapshot:/Reverb/Sewer Reverb", "{c107e28e-873b-4caa-9492-68fe72210c48}");
+		public static FMODEventData snapshot_Pause = new FMODEventData("snapshot:/Pause", "{e305a498-2b18-461d-b35c-6eaea45fafd6}");
+		public static FMODEventData snapshot_Reverb_Cave_Reverb = new FMODEventData("snapshot:/Reverb/Cave Reverb", "{b695eec0-fe78-4755-b806-9690c06dfc0b}");
+		public static FMODEventData snapshot_Test = new FMODEventData("snapshot:/Test", "{bae958d6-a263-4de0-9ddc-64e3d8644c76}");
+	}
+}
