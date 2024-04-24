dd264ee4ab37df0fa16529b1369e89f74942ecd2
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-04-24 14:02:03 +0600

    Updated samples

--- Assets/Packages/com.studio23.ss2.audiosystem.fmod/Samples/Demo1/FMOD_Data/FMODVCAList.cs
+++ Assets/Packages/com.studio23.ss2.audiosystem.fmod/Samples/Demo1/FMOD_Data/FMODVCAList.cs
@@ -4,8 +4,13 @@ namespace Studio23.SS2.AudioSystem.fmod
 {
 	public static class FMODVCAList
 	{
+		public static readonly string SFX = "vca:/SFX";
+		public static readonly string VO = "vca:/VO";
+		public static readonly string Master = "vca:/Master";
+		public static readonly string UI = "vca:/UI";
 		public static readonly string Environment = "vca:/Environment";
 		public static readonly string Sample = "vca:/Sample";
+		public static readonly string Music = "vca:/Music";
 		public static readonly string Player = "vca:/Player";
 		public static readonly string Equipment = "vca:/Equipment";
 	}
