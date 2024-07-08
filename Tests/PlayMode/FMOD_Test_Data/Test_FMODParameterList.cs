777a78c08f93bc3e52a5bf183a7221fe8ba89296
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-07-08 13:07:04 +0600

    Formatted full project

---
+++ Tests/PlayMode/FMOD_Test_Data/Test_FMODParameterList.cs
@@ -0,0 +1,24 @@
+namespace Studio23.SS2.AudioSystem.fmod.Tests
+{
+	public static class Test_FMODParameterList
+	{
+		public static class snapshot_Test
+		{
+			public static readonly string GlobalTest = "GlobalTest";
+		}
+		public static class Test
+		{
+			public static readonly string TestParameter = "TestParameter";
+		}
+		public static class Test_3
+		{
+			public static readonly string VolumeFadeOut = "VolumeFadeOut";
+			public static readonly string TestParameter = "TestParameter";
+			public static readonly string TestLabeled = "TestLabeled";
+		}
+		public static class Test_2
+		{
+			public static readonly string TestParameter = "TestParameter";
+		}
+	}
+}
