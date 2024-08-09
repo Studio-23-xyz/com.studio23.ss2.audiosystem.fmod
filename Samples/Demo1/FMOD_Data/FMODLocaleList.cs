32636eca3bd028fd487b3a189ba427ada3207c64
Rasheeq Kamal <riksaber@gmail.com>
2024-08-09 13:13:28 +0600

    Minor update to BanksManager

---
+++ Samples/Demo1/FMOD_Data/FMODLocaleList.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODLocaleList
+	{
+		public static Dictionary<string, string> LanguageList = new Dictionary<string, string>
+		{
+			{"Bangla (bn)", "DialogueTable_LOCALE_Bangla_(bn)"},
+			{"Chinese (cn)", "DialogueTable_LOCALE_Chinese_(cn)"},
+			{"English (en)", "DialogueTable_LOCALE_English_(en)"},
+			{"Japanese (jp)", "DialogueTable_LOCALE_Japanese_(jp)"},
+		};
+	}
+}
