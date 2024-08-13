4b28c51cc275af60b1ade06c733c4b30a3aa2794
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-08-13 15:27:57 +0600

    Removed unnecessary using statements

---
+++ Samples/Demo1/FMOD_Data/FMODBankList.cs
@@ -0,0 +1,16 @@
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODBankList
+	{
+		public static readonly string Master_strings = "Master.strings";
+		public static readonly string Dialogue = "Dialogue";
+		public static readonly string DialogueTable_LOCALE_Bangla_bn = "DialogueTable_LOCALE_Bangla_(bn)";
+		public static readonly string DialogueTable_LOCALE_Chinese_cn = "DialogueTable_LOCALE_Chinese_(cn)";
+		public static readonly string DialogueTable_LOCALE_English_en = "DialogueTable_LOCALE_English_(en)";
+		public static readonly string DialogueTable_LOCALE_Japanese_jp = "DialogueTable_LOCALE_Japanese_(jp)";
+		public static readonly string Master = "Master";
+		public static readonly string Sample = "Sample";
+		public static readonly string SFX = "SFX";
+		public static readonly string Test = "Test";
+	}
+}
