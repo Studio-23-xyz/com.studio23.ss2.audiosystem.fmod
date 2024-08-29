fb4ea86eb898d1f771660fe7ffa354020c565ea5
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-08-29 15:29:17 +0600

    Updated FMODEmitterData release method

---
+++ Samples/Demo1/FMOD_Data/FMODBank_Dialogue.cs
@@ -0,0 +1,9 @@
+using Studio23.SS2.AudioSystem.fmod.Data;
+
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODBank_Dialogue
+	{
+		public static string Dialogue_Dialogue = "{8e9f0b3d-5f42-422e-b43d-1b55fd43cdb2}";
+	}
+}
