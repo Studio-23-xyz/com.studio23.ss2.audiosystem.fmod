1ae3ddd3b4e84b82860d7817f475df866e477b0c
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2024-12-23 13:11:07 +0600

    Cleaned up some methods

---
+++ Samples/Demo1/FMOD_Data/FMODBank_Master.cs
@@ -0,0 +1,14 @@
+using Studio23.SS2.AudioSystem.fmod.Data;
+
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODBank_Master
+	{
+		public static string snapshot_Slow_Motion = "{6e8d8d09-12e0-48cf-b620-f9cc3bc478a3}";
+		public static string snapshot_Health_Low = "{87ea4134-fff1-4e9d-8f8b-294a2b518223}";
+		public static string snapshot_Reverb_Sewer_Reverb = "{c107e28e-873b-4caa-9492-68fe72210c48}";
+		public static string snapshot_Pause = "{e305a498-2b18-461d-b35c-6eaea45fafd6}";
+		public static string snapshot_Reverb_Cave_Reverb = "{b695eec0-fe78-4755-b806-9690c06dfc0b}";
+		public static string snapshot_Test = "{bae958d6-a263-4de0-9ddc-64e3d8644c76}";
+	}
+}
