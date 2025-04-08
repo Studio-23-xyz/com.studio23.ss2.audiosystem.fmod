7d017f2ff673f5dbef6cf24bd450a9952f54bc22
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2025-04-08 11:02:43 +0600

    Updated custom emitter

---
+++ Samples/Demo1/FMOD_Data/FMODBank_Test.cs
@@ -0,0 +1,11 @@
+using Studio23.SS2.AudioSystem.fmod.Data;
+
+namespace Studio23.SS2.AudioSystem.fmod
+{
+	public static class FMODBank_Test
+	{
+		public static string Test = "{7d4dbbf4-4e71-40ef-9152-be5069761cda}";
+		public static string Test_3 = "{074792d0-fd0d-44d4-ae87-f37f5820794a}";
+		public static string Test_2 = "{4bd7a2e2-4641-471d-ad1d-e8a4119afa57}";
+	}
+}
